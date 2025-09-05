df5a4595ea040b9befc663a263d6abc46d17d033
makowczak <mateuszmakowka100@gmail.com>
2025-09-05 15:46:58 +0200

    Testowanie zmiennych sucha

---
+++ liczbycalkowite.php
@@ -0,0 +1,12 @@
+<?php
+    // liczby całkowite
+    $liczba = 1234; // liczba dziesiętna
+    $oktalna = 0123; // ósemkowa
+    $hex = 0x1A;    // szesnastkowa
+    $binarny = 0b10101010; //binarna
+    $big = 1_123_567;   // duże liczby
+    echo $liczba . "<br>";
+    print $oktalna . "<br>";
+    echo $hex . "<br>" . $binarny. "<br>";
+    echo $big;
+?>
---
+++ typydanych.php
@@ -0,0 +1,9 @@
+<?php
+    $a_bool = TRUE;
+    $name = 'Karol';
+    $an_int = 12;
+    $pi = 3.1415926;
+    echo gettype( $a_bool )."<br>";
+    echo is_float( $pi )."<br>";
+    print var_dump($name)."<br>";
+?>
