e86b6c7cef0aaa26a6fcd7aa890878324b53cd4d
makowczak <106112048+makowczak@users.noreply.github.com>
2025-11-07 14:48:17 +0100

    Add files via upload
    07.11.25 egzaminek

---
+++ dzisiejszyegzamin/main.js
@@ -0,0 +1,9 @@
+function zmienStyl(color) {
+  let size = document.getElementById("procent").value;
+  let styl = document.getElementById("rodzaj").value;
+  let wynik = document.getElementById("wynik");
+
+  wynik.style.color = color;
+  wynik.style.fontSize = size + "%";
+  wynik.style.fontStyle = styl;
+}
