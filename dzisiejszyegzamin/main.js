c440c2dce95901bdb3110d88c6c166b8a0a26df2
makowczak <106112048+makowczak@users.noreply.github.com>
2025-11-07 14:49:17 +0100

    Delete dzisiejszyegzamin directory

--- dzisiejszyegzamin/main.js
+++
@@ -1,9 +0,0 @@
-function zmienStyl(color) {
-  let size = document.getElementById("procent").value;
-  let styl = document.getElementById("rodzaj").value;
-  let wynik = document.getElementById("wynik");
-
-  wynik.style.color = color;
-  wynik.style.fontSize = size + "%";
-  wynik.style.fontStyle = styl;
-}
